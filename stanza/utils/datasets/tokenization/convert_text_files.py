19c51871813572ac45e29bc9be66b7aa43b40489
John Bauer <horatio@gmail.com>
2022-09-13 22:29:37 -0700

    Process token + sentence files instead of just token files

--- stanza/utils/datasets/tokenization/convert_text_files.py
+++ stanza/utils/datasets/tokenization/convert_text_files.py
@@ -32,13 +32,25 @@ def read_tokens_file(token_file):
 
     return sentences
 
-def process_raw_file(text_file, token_file):
+def read_sentences_file(sentence_file):
+    sentences = []
+    with open(sentence_file, encoding="utf-8") as fin:
+        for line in fin:
+            line = line.strip()
+            if not line:
+                continue
+            sentences.append(line)
+    return sentences
+
+def process_raw_file(text_file, token_file, sentence_file):
     """
     Process a text file separated into a list of tokens using match_tokens_with_text from the tokenizer
 
     The tokens are one per line in the token_file
     The tokens in the token_file must add up to the text_file modulo whitespace.
-    Sentence breaks should be represented by blank lines between sentence
+
+    Sentences are also one per line in the sentence_file
+    These must also add up to text_file
 
     The return format is a list of list of conllu lines representing the sentences.
     The only fields set will be the token index, the token text, and possibly SpaceAfter=No
@@ -48,32 +60,52 @@ def process_raw_file(text_file, token_file):
         text = fin.read()
 
     tokens = read_tokens_file(token_file)
-    doc = match_tokens_with_text(tokens, text)
+    tokens = [[token for sentence in tokens for token in sentence]]
+    tokens_doc = match_tokens_with_text(tokens, text)
 
+    assert len(tokens_doc.sentences) == 1
+    assert len(tokens_doc.sentences[0].tokens) == len(tokens[0])
+
+    sentences = read_sentences_file(sentence_file)
+    sentences_doc = match_tokens_with_text([sentences], text)
+
+    assert len(sentences_doc.sentences) == 1
+    assert len(sentences_doc.sentences[0].tokens) == len(sentences)
+
+    start_token_idx = 0
     sentences = []
-    for sent_idx, sentence in enumerate(doc.sentences):
+    for sent_idx, sentence in enumerate(sentences_doc.sentences[0].tokens):
         tokens = []
         tokens.append("# sent_id = %d" % (sent_idx+1))
-        tokens.append("# text = %s" % text[sentence.tokens[0].start_char:sentence.tokens[-1].end_char].replace("\n", " "))
-        for token_idx, token in enumerate(sentence.tokens):
-            #text = token.text
-            if token_idx == len(sentence.tokens) - 1 and sent_idx == len(doc.sentences) - 1:
+        tokens.append("# text = %s" % text[sentence.start_char:sentence.end_char].replace("\n", " "))
+        token_idx = 0
+        while token_idx + start_token_idx < len(tokens_doc.sentences[0].tokens):
+            token = tokens_doc.sentences[0].tokens[token_idx + start_token_idx]
+            if token.start_char >= sentence.end_char:
+                # have reached the end of this sentence
+                # continue with the next sentence
+                start_token_idx += token_idx
+                break
+
+            if token_idx + start_token_idx == len(tokens_doc.sentences[0].tokens) - 1:
+                # definitely the end of the document
                 space_after = True
-            elif token_idx == len(sentence.tokens) - 1:
-                space_after = not token.end_char == doc.sentences[sent_idx+1].tokens[0].start_char
+            elif token.end_char == tokens_doc.sentences[0].tokens[token_idx + start_token_idx + 1].start_char:
+                space_after = False
             else:
-                space_after = not token.end_char == sentence.tokens[token_idx+1].start_char
+                space_after = True
             token = [str(token_idx+1), token.text] + ["_"] * 7 + ["_" if space_after else "SpaceAfter=No"]
             assert len(token) == 10, "Token length: %d" % len(token)
             token = "\t".join(token)
             tokens.append(token)
+            token_idx += 1
         sentences.append(tokens)
     return sentences
 
-def extract_sentences(text_files, token_files):
+def extract_sentences(text_files, token_files, sentence_files):
     sentences = []
-    for text_file, token_file in zip(text_files, token_files):
-        sentences.extend(process_raw_file(text_file, token_file))
+    for text_file, token_file, sentence_file in zip(text_files, token_files, sentence_files):
+        sentences.extend(process_raw_file(text_file, token_file, sentence_file))
     return sentences
 
 def split_sentences(sentences, train_split=0.8, dev_split=0.1):
@@ -101,20 +133,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--text_files', type=str, default="extern_data/sindhi/tokenization/FolkStory1.txt,extern_data/sindhi/tokenization/String1.txt", help="Where to find the text files")
     parser.add_argument('--token_files', type=str, default="extern_data/sindhi/tokenization/tkns_FolkStory1.txt,extern_data/sindhi/tokenization/tkns_String1.txt", help="Where to find the token files")
+    parser.add_argument('--sentence_files', type=str, default="extern_data/sindhi/tokenization/stns_FolkStory1.txt,extern_data/sindhi/tokenization/stns_String1.txt", help="Where to find the sentence files")
     parser.add_argument('--output_path', type=str, default="data/tokenize", help="Where to output the results")
     parser.add_argument('--dataset', type=str, default="sd_isra", help="What name to give this dataset")
     args = parser.parse_args()
 
     text_files = args.text_files.split(",")
     token_files = args.token_files.split(",")
+    sentence_files = args.sentence_files.split(",")
 
     tokenizer_dir = args.output_path
     short_name = args.dataset  # todo: convert a full name?
 
     if len(text_files) != len(token_files):
         raise ValueError("Expected same number of text and token files")
+    if len(text_files) != len(sentence_files):
+        raise ValueError("Expected same number of text and sentence files")
 
-    sentences = extract_sentences(text_files, token_files)
+    sentences = extract_sentences(text_files, token_files, sentence_files)
     splits = split_sentences(sentences)
 
     for dataset, shard in zip(splits, SHARDS):
